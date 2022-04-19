c11a9166f9e90b1be2fd02280eb9f704aa1bd622
anothergreenhat <jamadera28@gmail.com>
2022-04-18 22:38:02 -0400

    add insert smoothie

--- create.sql
+++ create.sql
@@ -7,7 +7,7 @@ DROP SEQUENCE seqSmID;
 CREATE TABLE smoothie(
 	smoothie_id			Int	NOT NULL,
 	price			    number(10,2)   NOT NULL,
-	size_			    varchar(6),
+	ssize			    varchar(6),
     fruit_one			varchar(15) NOT NULL,
     fruit_two           varchar(15) NULL,
     fruit_three         varchar(15) NULL,
@@ -16,16 +16,13 @@ CREATE TABLE smoothie(
     protein             varchar(4) NULL,
     boost               varchar(10) NULL,
     frozen_sweet        varchar(20) NULL,
-    constraint size_c check(size_ in ('small','medium','large') ),
-    constraint fruit_c check (fruit_one in ('orange','banana','apple','peach', 'strawberry', 'blueberry','pineapple','mango') ),
-    constraint veg_c check (veg_one in ('kale','spinach','cucumber','beet') ),
+    constraint size_c check(ssize in ('small','medium','large') ),
+    constraint fruit_c check (fruit_one in ('orange','banana','apple','peach', 'strawberry', 'blueberry','pineapple','mango') and fruit_two in ('orange','banana','apple','peach', 'strawberry', 'blueberry','pineapple','mango') and fruit_three in ('orange','banana','apple','peach', 'strawberry', 'blueberry','pineapple','mango')),
+    constraint veg_c check (veg_one in ('kale','spinach','cucumber','beet') and veg_two in ('kale','spinach','cucumber','beet') ),
     constraint protein_c check (protein in ('whey','soy')),
     constraint boost_c check (boost in ('vitamin_c','zinc','matcha') ),
     constraint frozen_sweet_c check (frozen_sweet in ('vanilla_yogurt','greek_yogurt','orange_sherbet','raspberry_sherbet','lime_serbet') ),
 	primary key (smoothie_id)
 );
-
-
-
 CREATE SEQUENCE seqSmID INCREMENT BY 1 START WITH 1;
 
---
+++ insert.sql
@@ -0,0 +1,4 @@
+--Benjamin Lucas, Joseph Madera, Hannah Packard, Abbigail Thompson
+--Insert Data
+
+insert into smoothie values(seqSmID.nextVal,7.99,'small','orange','blueberry',null,'kale',null,null,'zinc',null);
