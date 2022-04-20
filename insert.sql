5785fc07d60d95725b5239f08679837a99873ea5
anothergreenhat <jamadera28@gmail.com>
2022-04-20 11:20:00 -0400

    inserted customer test data

--- insert.sql
+++ insert.sql
@@ -3,3 +3,6 @@
 cl scr;
 insert into smoothie values(seqSmID.nextVal,7.99,'small','orange','blueberry',null,'kale',null,null,'zinc',null);
 insert into rewards values(seqRID.nextVal, null, 10); -- need to automatically decrement rewards.no_smoothies_to_free based on insert or update of order
+insert into customer values(seqCID.nextVal, null, 'John Doe', TO_DATE('01-01-2000', 'MM-DD-YYYY'),'test@villanova.edu');
+
+
