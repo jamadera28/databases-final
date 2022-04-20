e6d34fc8b15dc9629a6551fb1435658692d1ee6e
anothergreenhat <jamadera28@gmail.com>
2022-04-20 12:03:03 -0400

    added price retrieval

--- insert.sql
+++ insert.sql
@@ -8,10 +8,21 @@ insert into customer values(seqCID.nextVal, null, 'John Doe', TO_DATE('01-01-200
 insert into smoothista values(seqSmID.nextVal, null, null, 'Jane Doe');
 insert into smoothie_store values(seqSsID.nextVal, 1, 'Bryn Mawr');
 insert into orders values(
-    seqOID.nextVal, 1, 1, 1, 1, current_date, 'pickup', null);
+    seqOID.nextVal, 1, 1, 1, 1, current_date, 'pickup');
     
 update rewards set customer_id = 1;
 update customer set rewards_id = 1;
 update smoothista set smoothie_store_id = 1;
 
 update rewards set no_smoothies_to_free = 0;
+
+insert into smoothie values(seqSID.nextVal,12.29,'medium','strawberry','blueberry','mango','kale','cucumber','whey',null,null);
+insert into smoothie values(seqSID.nextVal,4.99,'small','blueberry',null,null,'kale',null,null,null,null);
+insert into smoothie values(seqSID.nextVal,10.38,'large','banana','mango',null,'spinach','kale','soy','vitamin_c','raspberry_sherbet');
+
+insert into orders values(
+    seqOID.nextVal, 1, 2, 1, 1, current_date, 'pickup');
+insert into orders values(
+    seqOID.nextVal, 1, 3, 1, 1, current_date, 'delivery');
+insert into orders values(
+    seqOID.nextVal, 1, 4, 1, 1, current_date, 'curbside');
--- retrieve.sql
+++ retrieve.sql
@@ -6,3 +6,7 @@
 select sname from smoothista natural join orders
 where customer_id in (select customer_id from customer natural join rewards
     where no_smoothies_to_free = 0);
+    
+select avg(price) from smoothie natural join orders
+where smoothie_store_id in (select smoothie_store_id from smoothie_store
+    where slocation = 'Bryn Mawr');
