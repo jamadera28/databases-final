cfb4260f6b32bb64e3b5022656f45486cc77a4db
anothergreenhat <jamadera28@gmail.com>
2022-04-21 19:27:49 -0400

    use case group by store avg price

--- retrieve.sql
+++ retrieve.sql
@@ -7,7 +7,6 @@ select sname from smoothista natural join orders
 where customer_id in (select customer_id from customer natural join rewards
     where no_smoothies_to_free = 0);
     
+-- select the average price of smoothie orders for all stores
 select round(avg(price),2) as "Avg Smoothie Price", slocation as "Store" from smoothie, smoothie_store natural join orders
-where smoothie_store_id in (select smoothie_store_id from smoothie_store
-    where slocation = 'Bryn Mawr')
 group by slocation;    
