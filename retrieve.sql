7e0eeea68c088bb5b5f95e5c5eb1ff97086fd789
blucas6 <66273291+blucas6@users.noreply.github.com>
2022-04-23 21:23:51 -0400

    added additional queries

--- retrieve.sql
+++ retrieve.sql
@@ -1,12 +1,22 @@
 --Benjamin Lucas, Joseph Madera, Hannah Packard, Abbigail Thompson
 --Retrieve Data
 
-
 -- select the smoothista that made a smoothie for a customer with a free smoothie available
-select sname from smoothista natural join orders
+select sname as "Smoothista Made Free Smoothie" from smoothista natural join orders
 where customer_id in (select customer_id from customer natural join rewards
     where no_smoothies_to_free = 0);
     
 -- select the average price of smoothie orders for all stores
 select round(avg(price),2) as "Avg Smoothie Price", slocation as "Store" from smoothie, smoothie_store natural join orders
 group by slocation;    
+
+-- select orders per store
+select count(o.order_id) as "Orders per Store", s.slocation as "Location"
+from orders o
+INNER JOIN smoothie_store s ON s.smoothie_store_id = o.smoothie_store_id
+group by s.smoothie_store_id, s.slocation;
+
+-- customer's favorite smoothies
+select c.cname as "Customer", s.smoothie_id as "Smoothie"
+from customer c, smoothie s, orders o
+where o.customer_id = c.customer_id and c.customer_id = 4;
